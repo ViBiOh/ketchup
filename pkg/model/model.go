30efb32f84c34702c397d2b4594b79bef66270f4
Vincent Boutour <bob@vibioh.fr>
2020-04-19 16:20:48 +0200

    refactor: Harmonizing frontend with hue

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -4,3 +4,25 @@ package model
 type RowScanner interface {
 	Scan(...interface{}) error
 }
+
+// Message for render
+type Message struct {
+	Level   string
+	Content string
+}
+
+// NewSuccessMessage create a success message
+func NewSuccessMessage(content string) Message {
+	return Message{
+		Level:   "success",
+		Content: content,
+	}
+}
+
+// NewErrorMessage create a error message
+func NewErrorMessage(content string) Message {
+	return Message{
+		Level:   "error",
+		Content: content,
+	}
+}
