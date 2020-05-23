1af24ac8b40784ac56fd9ad1d7e72c7016a018c3
Vincent Boutour <bob@vibioh.fr>
2020-05-23 18:52:28 +0200

    chore(db): Migrating auth database

--- migration_2020_05_23_1.sql
+++ migration_2020_05_23_1.sql
@@ -1,4 +1,9 @@
--- create schema
+CREATE SCHEMA auth;
+
+ALTER TABLE login_profile SET SCHEMA auth;
+ALTER TABLE profile SET SCHEMA auth;
+ALTER TABLE login SET SCHEMA auth;
+
 CREATE SCHEMA ketchup;
 
 ALTER TABLE "user" SET SCHEMA ketchup;
