d303c7ccf20f89e3525fdf3fcf790db89b3783ec
Vincent Boutour <vboutour@meilleursagents.com>
2020-05-25 12:06:38 +0200

    feat(semver): Ignoring test version

--- pkg/semver/semver.go
+++ pkg/semver/semver.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	semverMatcher       = regexp.MustCompile(`(?i)^[a-zA-Z]*([0-9]+)\.([0-9]+)(?:\.([0-9]+))?(?:$|(?:[+-](.*)))`)
-	ignoredSemverDetail = []string{"rc", "beta"}
+	ignoredSemverDetail = []string{"rc", "beta", "test"}
 
 	// NoneVersion is the empty semver
 	NoneVersion = Version{}
