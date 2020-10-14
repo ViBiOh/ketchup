f7b80e1a97e4cc4e74ee5c5d7463e4b796c35f4a
Vincent Boutour <bob@vibioh.fr>
2020-10-14 20:38:24 +0200

    test: Improve coverage of repository package

---
+++ pkg/renderer/app_test.go
@@ -0,0 +1,41 @@
+package renderer
+
+import "testing"
+
+func TestMin(t *testing.T) {
+	type args struct {
+		a uint64
+		b uint64
+	}
+
+	var cases = []struct {
+		intention string
+		args      args
+		want      uint64
+	}{
+		{
+			"a",
+			args{
+				a: 1,
+				b: 2,
+			},
+			1,
+		},
+		{
+			"b",
+			args{
+				a: 3,
+				b: 2,
+			},
+			2,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.intention, func(t *testing.T) {
+			if got := min(tc.args.a, tc.args.b); got != tc.want {
+				t.Errorf("min() = %d, want %d", got, tc.want)
+			}
+		})
+	}
+}
