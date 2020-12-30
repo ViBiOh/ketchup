5ee0d62d0e2ebcdd493b0972dedaf9116239de8e
Vincent Boutour <bob@vibioh.fr>
2020-12-30 11:45:53 +0100

    fix: Fixing migration script [skip-ci]

--- sql/migration_2020_12_30_1.sql
+++ sql/migration_2020_12_30_1.sql
@@ -1,2 +1,2 @@
-ALTER TYPE ketchup.repository_type RENAME TO ketchup.repository_kind
+ALTER TYPE ketchup.repository_type RENAME TO repository_kind;
 ALTER TABLE ketchup.repository RENAME COLUMN type TO kind;
