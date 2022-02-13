578f47cdf122d239781ee1229841d5798ab22b8b
Vincent Boutour <bob@vibioh.fr>
2022-02-13 11:18:13 +0100

    chore(deps): Bumping deps for using new renderer interface
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/ketchup/renderer.go
+++ pkg/ketchup/renderer.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ViBiOh/httputils/v4/pkg/cache"
 	"github.com/ViBiOh/httputils/v4/pkg/logger"
 	"github.com/ViBiOh/httputils/v4/pkg/query"
+	"github.com/ViBiOh/httputils/v4/pkg/renderer"
 	"github.com/ViBiOh/ketchup/pkg/model"
 )
 
@@ -16,17 +17,17 @@ const (
 )
 
 // PublicTemplateFunc rendering public GUI
-func (a App) PublicTemplateFunc(_ http.ResponseWriter, r *http.Request) (string, int, map[string]interface{}, error) {
+func (a App) PublicTemplateFunc(_ http.ResponseWriter, r *http.Request) (renderer.Page, error) {
 	securityPayload, err := a.generateToken(r.Context())
 	if err != nil {
-		return "", http.StatusInternalServerError, nil, err
+		return renderer.NewPage("", http.StatusInternalServerError, nil), err
 	}
 
-	return "public", http.StatusOK, map[string]interface{}{
+	return renderer.NewPage("public", http.StatusOK, map[string]interface{}{
 		"Security": securityPayload,
 		"Suggests": a.suggests(r.Context(), nil, 3),
 		"Root":     "/",
-	}, nil
+	}), nil
 }
 
 func min(a, b uint64) uint64 {
@@ -37,15 +38,15 @@ func min(a, b uint64) uint64 {
 }
 
 // AppTemplateFunc rendering private GUI
-func (a App) AppTemplateFunc(_ http.ResponseWriter, r *http.Request) (string, int, map[string]interface{}, error) {
+func (a App) AppTemplateFunc(_ http.ResponseWriter, r *http.Request) (renderer.Page, error) {
 	pagination, err := query.ParsePagination(r, 100, 100)
 	if err != nil {
-		return "", http.StatusBadRequest, nil, err
+		return renderer.NewPage("", http.StatusBadRequest, nil), err
 	}
 
 	ketchups, _, err := a.ketchupService.List(r.Context(), pagination.PageSize, pagination.Last)
 	if err != nil {
-		return "", http.StatusInternalServerError, nil, err
+		return renderer.NewPage("", http.StatusInternalServerError, nil), err
 	}
 
 	content := map[string]interface{}{
@@ -64,7 +65,7 @@ func (a App) AppTemplateFunc(_ http.ResponseWriter, r *http.Request) (string, in
 		content["Suggests"] = a.suggests(r.Context(), ketchupIds, min(suggestThresold-ketchupsCount, suggestThresold))
 	}
 
-	return "ketchup", http.StatusOK, content, nil
+	return renderer.NewPage("ketchup", http.StatusOK, content), nil
 }
 
 func (a App) suggests(ctx context.Context, ignoreIds []uint64, count uint64) []model.Repository {
