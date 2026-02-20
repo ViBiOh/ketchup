20fb79b8894f1e2f09ae5ebf97dd48f8ea70be1f
Vincent Boutour <bob@vibioh.fr>
2026-02-20 12:48:41 +0100

    feat: Remove specific pattern for helm in the scheduler
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/notifier/releases.go
+++ pkg/notifier/releases.go
@@ -12,31 +12,12 @@ import (
 )
 
 func (s Service) getNewReleases(ctx context.Context) ([]model.Release, error) {
-	var releases, helmReleases []model.Release
+	releases, err := s.getNewStandardReleases(ctx)
+	if err != nil {
+		slog.LogAttrs(ctx, slog.LevelError, "fetch releases", slog.Any("error", err))
+	}
 
-	wg := concurrent.NewLimiter(2)
-
-	wg.Go(func() {
-		var err error
-
-		releases, err = s.getNewStandardReleases(ctx)
-		if err != nil {
-			slog.LogAttrs(ctx, slog.LevelError, "fetch standard releases", slog.Any("error", err))
-		}
-	})
-
-	wg.Go(func() {
-		var err error
-
-		helmReleases, err = s.getNewHelmReleases(ctx)
-		if err != nil {
-			slog.LogAttrs(ctx, slog.LevelError, "fetch helm releases", slog.Any("error", err))
-		}
-	})
-
-	wg.Wait()
-
-	return append(releases, helmReleases...), nil
+	return releases, nil
 }
 
 func (s Service) getNewStandardReleases(ctx context.Context) ([]model.Release, error) {
@@ -110,94 +91,6 @@ func (s Service) getNewRepositoryReleases(ctx context.Context, repo model.Reposi
 
 	for pattern, version := range versions {
 		releases = appendVersion(ctx, releases, version, repo, pattern, repo.Versions[pattern])
-	}
-
-	return releases
-}
-
-func (s Service) getNewHelmReleases(ctx context.Context) ([]model.Release, error) {
-	var newReleases []model.Release
-	var count uint64
-	var last string
-
-	for {
-		repositories, err := s.repository.ListByKinds(ctx, pageSize, last, model.Helm)
-		if err != nil {
-			return nil, fmt.Errorf("fetch Helm repositories: %w", err)
-		}
-
-		repoCount := len(repositories)
-		if repoCount == 0 {
-			break
-		}
-
-		repoName := repositories[0].Name
-		repoWithNames := make(map[string]model.Repository)
-
-		for _, repo := range repositories {
-			count++
-
-			if repo.Name != repoName {
-				newReleases = append(newReleases, s.getFetchHelmSources(ctx, repoWithNames)...)
-
-				repoName = repo.Name
-				repoWithNames = make(map[string]model.Repository)
-			}
-
-			repoWithNames[repo.Part] = repo
-		}
-
-		newReleases = append(newReleases, s.getFetchHelmSources(ctx, repoWithNames)...)
-
-		if repoCount < int(pageSize) {
-			break
-		}
-
-		lastRepo := repositories[len(repositories)-1]
-		last = fmt.Sprintf("%s|%s", lastRepo.Name, lastRepo.Part)
-	}
-
-	slog.LogAttrs(ctx, slog.LevelInfo, "Helm repositories checked", slog.Uint64("count", count), slog.Int("new", len(newReleases)))
-	return newReleases, nil
-}
-
-func (s Service) getFetchHelmSources(ctx context.Context, repos map[string]model.Repository) []model.Release {
-	if len(repos) == 0 {
-		return nil
-	}
-
-	var url string
-
-	charts := make(map[string][]string)
-	for _, repo := range repos {
-		if len(url) == 0 {
-			url = repo.Name
-		}
-
-		index := 0
-		patterns := make([]string, len(repo.Versions))
-		for pattern := range repo.Versions {
-			patterns[index] = pattern
-			index++
-		}
-
-		charts[repo.Part] = patterns
-	}
-
-	values, err := s.helm.FetchIndex(ctx, url, charts)
-	if err != nil {
-		slog.LogAttrs(ctx, slog.LevelError, "fetch helm index", slog.String("url", url), slog.Any("error", err))
-		return nil
-	}
-
-	var releases []model.Release
-
-	for chartName, patterns := range values {
-		repo := repos[chartName]
-
-		for repoPattern, repoVersionName := range repo.Versions {
-			releases = appendVersion(ctx, releases, patterns[repoPattern], repo, repoPattern, repoVersionName)
-		}
 	}
 
 	return releases
