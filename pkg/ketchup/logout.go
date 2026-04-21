3aea5ce4c268423eec8acbc90ffdf1a195290caf
Vincent Boutour <bob@vibioh.fr>
2026-04-21 09:25:27 +0200

    fix(ketchup): Logout redirection to clear cookie
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/ketchup/logout.go
+++ pkg/ketchup/logout.go
@@ -4,11 +4,19 @@ import (
 	"net/http"
 
 	"github.com/ViBiOh/httputils/v4/pkg/renderer"
+	"github.com/ViBiOh/httputils/v4/pkg/telemetry"
 )
 
 func (s Service) Logout() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		telemetry.SetRouteTag(ctx, "/logout")
+
 		s.logout.Logout(w, r)
-		s.renderer.Redirect(w, r, "/", renderer.NewSuccessMessage("Logged out"))
+
+		s.renderer.Serve(w, r, renderer.NewPage("auth", http.StatusOK, map[string]any{
+			"Redirect": "/",
+			"Message":  renderer.NewSuccessMessage("Logout success!"),
+		}))
 	})
 }
